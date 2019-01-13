b8cccab2645861e9f2b509f6766d2c7c11ead180
Nik Ambukovski <lazokin@hotmail.com>
2019-01-13 15:24:00 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Recipe Book
+A Spring Boot version of the Recipe Book application from the Udemy course Angular 7. Built using Travis CI.
---
+++ src/main/java/com/lazokin/recipebook/SpringRecipeBookApplication.java
@@ -0,0 +1,14 @@
+package com.lazokin.recipebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecipeBookApplication.class, args);
+	}
+
+}
+
